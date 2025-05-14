e80a2e150f5279349dc304bfd3c181e5ac7003fb
Javier Rambaldo <65971139+javirambo@users.noreply.github.com>
2025-05-13 21:28:02 -0300

    v5

--- test/color.h
+++
@@ -1,5 +0,0 @@
-#include <Arduino.h>
-
-void HSVtoRGB(float &fR, float &fG, float &fB, float &fH, float &fS, float &fV);
-
-void RGBtoHSV(float &fR, float &fG, float fB, float &fH, float &fS, float &fV);
